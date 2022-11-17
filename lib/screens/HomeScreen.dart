521e0e4fcd239d9d2f4b47d1496d069d07664830
gchora01 <gchora01@ucy.ac.cy>
2022-11-17 20:59:01 +0200

    Added button for moving on StoresScreen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shoppy/screens/StoresScreen.dart';
+
+import '../styles/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,8 +14,41 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[300],
-      child: const Center(
-        child: Text('Home Page'),
+      child: Scaffold(
+        //touto to button en gia na pernei sto sign up page gia testing
+       body: Column(
+          children:  [
+              TextButton(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: RichText(
+                              softWrap: true,
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: "Supermarkets",
+                                    style: TextStyle(
+                                        fontSize: 25, color: ShoppyColors.blue),
+                                  ),
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.arrow_right_alt_rounded,
+                                      size: 25,
+                                      color: ShoppyColors.blue,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (_) => const StoresScreen()),
+                            );
+                          },
+                        ),
+        ],)
         ),
     );
   }
