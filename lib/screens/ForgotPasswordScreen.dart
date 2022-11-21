56a5fa3efdac94a01616603fa7828297b64559f9
gchora01 <gchora01@ucy.ac.cy>
2022-11-21 10:26:08 +0200

    Modified StoresScreen and created ForgotPasswordScreen

--- lib/screens/ForgotPasswordScreen.dart
+++ lib/screens/ForgotPasswordScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../styles/colors.dart';
+import 'SignUpScreen.dart';
+
 class ForgotPasswordScreen extends StatefulWidget {
   @override
   _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
@@ -9,10 +12,42 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[300],
-      child: const Center(
-        child: Text('Home Page'),
+      
+      decoration:  BoxDecoration(
+        color:ShoppyColors.blue,
+        image: const DecorationImage(
+            image: AssetImage("asset/images/logo.png",) ),
       ),
-    );
-  }
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+          
+                  body:    Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Don't have an account?",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => const SignUp()));
+                                },
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    color: ShoppyColors.red,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              )
+                    ],
+                  )
+                  ));
+}
 }
