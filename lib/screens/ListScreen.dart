ef82986a4292d6cbd2850cdf87d475dbc51d6e7b
cpaphi01 <cpaphi01@ucy.ac.cy>
2022-11-22 18:33:11 +0200

    shopping list update

--- lib/screens/ListScreen.dart
+++ lib/screens/ListScreen.dart
@@ -5,6 +5,7 @@ import 'package:shoppy/DataController.dart';
 import 'package:shoppy/models/ItemStore.dart';
 import 'package:shoppy/models/User.dart';
 import 'package:shoppy/styles/colors.dart';
+import 'dart:ffi';
 
 class ListScreen extends StatefulWidget {
   @override
@@ -36,6 +37,7 @@ class _ListScreenState extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ShoppyColors.gray,
       body: SmartRefresher(
         enablePullDown: true,
         enablePullUp: true,
@@ -47,7 +49,8 @@ class _ListScreenState extends State<ListScreen> {
           itemCount: _shoppingList.length,
           itemBuilder: (BuildContext context, int indexS) {
             return Card(
-              color: ShoppyColors.gray,
+              elevation: 0,
+              color: Colors.transparent,
               child: Column(
                 children: [
                   Row(
@@ -55,7 +58,7 @@ class _ListScreenState extends State<ListScreen> {
                     children: [
                       Image.network(
                         _shoppingList[indexS].store.storeImage,
-                        height: 30,
+                        height: 50,
                         width: 80,
                       ),
                       //  image: AssetImage(products[index].image.toString()),
@@ -71,6 +74,17 @@ class _ListScreenState extends State<ListScreen> {
                               value: _shoppingList[indexS].isChecked,
                               onChanged: (v) {
                                 setState(() {
+                                  for (var i = 0;
+                                      i <
+                                          _shoppingList[indexS]
+                                              .itemOffers
+                                              .length;
+                                      i++) {
+                                    _shoppingList[indexS]
+                                        .itemOffers[i]
+                                        .isChecked = v!;
+                                  }
+
                                   _shoppingList[indexS].isChecked = v!;
                                 });
                               },
@@ -90,102 +104,179 @@ class _ListScreenState extends State<ListScreen> {
                     itemCount: _shoppingList[indexS].itemOffers.length,
                     itemBuilder: (BuildContext context, int index) {
                       //List<bool> offers = List.filled(2, false);
-                      return Card(
-                        color: ShoppyColors.gray,
-                        elevation: 5.0,
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 40),
-                          child: Row(
-                            children: [
-                              Checkbox(
-                                shape: const RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(5.0))),
-                                value: _shoppingList[indexS].isChecked ||
-                                    _shoppingList[indexS]
-                                        .itemOffers[index]
-                                        .isChecked,
-                                onChanged: (v) {
-                                  setState(() {
-                                    print(indexS);
-                                    _shoppingList[indexS]
-                                        .itemOffers[index]
-                                        .isChecked = v!;
-                                  });
-                                },
-                              ),
-                              Image.network(
-                                _shoppingList[indexS]
-                                    .itemOffers[index]
-                                    .offer
-                                    .product
-                                    .productImage,
-                                height: 30,
-                                width: 80,
-                              ),
-                              SizedBox(
-                                width: 130,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    RichText(
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 1,
-                                      text: TextSpan(
-                                          text:
-                                              'Name: ${_shoppingList[indexS].itemOffers[index].offer.product.productName}\n',
-                                          style: TextStyle(
-                                              color: Colors.blueGrey.shade800,
-                                              fontSize: 16.0),
-                                          children: const [
-                                            TextSpan(
-                                                text: 'ProductName\n',
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                          ]),
-                                    ),
-                                    RichText(
-                                      maxLines: 10,
-                                      text: TextSpan(
-                                          text:
-                                              'Unit: ${_shoppingList[indexS].itemOffers[index].quantity}\n',
-                                          style: TextStyle(
-                                              color: Colors.blueGrey.shade800,
-                                              fontSize: 16.0),
-                                          children: [
-                                            TextSpan(
-                                                text:
-                                                    'Brand: ${_shoppingList[indexS].itemOffers[index].offer.product.brand}\n',
-                                                style: const TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                          ]),
-                                    ),
-                                    RichText(
-                                      maxLines: 10,
-                                      text: TextSpan(
-                                          text:
-                                              'Price: ${_shoppingList[indexS].itemOffers[index].offer.price}'
-                                              r"€",
-                                          style: TextStyle(
-                                              color: Colors.blueGrey.shade800,
-                                              fontSize: 16.0),
-                                          children: const [
-                                            TextSpan(
-                                                text: 'ProductPrice\n',
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                          ]),
-                                    ),
-                                  ],
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 2.0),
+                        child: Card(
+                          color: Colors.white,
+                          elevation: 5.0,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10, bottom: 10),
+                            child: Row(
+                              children: [
+                                Checkbox(
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(5.0))),
+                                  value: _shoppingList[indexS]
+                                      .itemOffers[index]
+                                      .isChecked,
+                                  onChanged: (v) {
+                                    setState(() {
+                                      print(indexS);
+                                      _shoppingList[indexS]
+                                          .itemOffers[index]
+                                          .isChecked = v!;
+                                      if (v! == false) {
+                                        _shoppingList[indexS].isChecked = false;
+                                      }
+                                    });
+                                  },
                                 ),
-                              ),
-                            ],
+                                Image.network(
+                                  _shoppingList[indexS]
+                                      .itemOffers[index]
+                                      .offer
+                                      .product
+                                      .productImage,
+                                  height: 30,
+                                  width: 80,
+                                ),
+                                SizedBox(
+                                  width: 120,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(
+                                        height: 5.0,
+                                      ),
+                                      // RichText(
+                                      //   //overflow: TextOverflow.ellipsis,
+                                      //   //maxLines: 1,
+                                      //   text: TextSpan(
+                                      //       text:
+                                      //           '${_shoppingList[indexS].itemOffers[index].offer.product.productName}\n',
+                                      //       style: TextStyle(
+                                      //           color: Colors.blueGrey.shade800,
+                                      //           fontSize: 16.0),
+                                      //       children: const [
+                                      //         TextSpan(
+                                      //             text: 'ProductName\n',
+                                      //             style: TextStyle(
+                                      //                 fontWeight:
+                                      //                     FontWeight.bold)),
+                                      //       ]),
+                                      // ),
+                                      RichText(
+                                        maxLines: 10,
+                                        text: TextSpan(
+                                            // text:
+                                            //     'Qty: ${_shoppingList[indexS].itemOffers[index].quantity}\n',
+                                            // style: TextStyle(
+                                            //     color:
+                                            //         Colors.blueGrey.shade800,
+                                            //     fontSize: 16.0),
+                                            children: [
+                                              TextSpan(
+                                                text:
+                                                    '${_shoppingList[indexS].itemOffers[index].offer.product.productName}\n',
+                                                style: TextStyle(
+                                                    color: Colors
+                                                        .blueGrey.shade800,
+                                                    fontSize: 16.0,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              (_shoppingList[indexS]
+                                                          .itemOffers[index]
+                                                          .offer
+                                                          .product
+                                                          .brand !=
+                                                      '')
+                                                  ? TextSpan(
+                                                      text:
+                                                          '${_shoppingList[indexS].itemOffers[index].offer.product.brand}\n',
+                                                      style: TextStyle(
+                                                          color: Colors.blueGrey
+                                                              .shade800,
+                                                          fontSize: 16.0))
+                                                  : const TextSpan(),
+                                              TextSpan(
+                                                text:
+                                                    'Qty: ${_shoppingList[indexS].itemOffers[index].quantity}\n',
+                                                style: TextStyle(
+                                                    color: Colors
+                                                        .blueGrey.shade800,
+                                                    fontSize: 16.0),
+                                              ),
+                                              TextSpan(
+                                                text:
+                                                    '€${_shoppingList[indexS].itemOffers[index].offer.price}',
+                                                style: TextStyle(
+                                                    color: Colors
+                                                        .blueGrey.shade800,
+                                                    fontSize: 16.0),
+                                              )
+                                            ]),
+                                      ),
+                                      // RichText(
+                                      //   maxLines: 10,
+                                      //   text: TextSpan(
+                                      //       text:
+                                      //           'Price: ${_shoppingList[indexS].itemOffers[index].offer.price}'
+                                      //           r"€",
+                                      //       style: TextStyle(
+                                      //           color:
+                                      //               Colors.blueGrey.shade800,
+                                      //           fontSize: 16.0),
+                                      //       children: const [
+
+                                      //         TextSpan(
+                                      //             text: 'ProductPrice\n',
+                                      //             style: TextStyle(
+                                      //                 fontWeight:
+                                      //                     FontWeight.bold)),
+                                      //       ]),
+                                      // ),
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          deleteOfferFromList(indexS, index);
+
+                                          _shoppingList = localUser.itemsInCart;
+                                        });
+                                      },
+                                      child: const Icon(Icons.delete),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          updateQtyList(indexS, index, 1);
+
+                                          _shoppingList = localUser.itemsInCart;
+                                        });
+                                      },
+                                      child: const Icon(Icons.arrow_upward),
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            updateQtyList(indexS, index, -1);
+
+                                            _shoppingList =
+                                                localUser.itemsInCart;
+                                          });
+                                        },
+                                        child: const Icon(Icons.arrow_downward))
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       );
